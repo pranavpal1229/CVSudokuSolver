cde08827ab0c2e468066c883f2c1397a0fda2461
Pranav <pranavpal@uchicago.edu>
2025-02-21 02:33:44 -0600

    finished sudoku

--- cv.py
+++ cv.py
@@ -7,8 +7,6 @@ cap.set(4, 480)
 
 last_grid = None  #needing this for constant flickering
 
-
-
 def getContours(img, original_img):
     global last_grid
 
@@ -25,17 +23,41 @@ def getContours(img, original_img):
             if len(approx) == 4 and area > max_area:
                 largest_contour = approx
                 max_area = area #updating area
-            
-            if largest_contour is not None:
-                last_grid = largest_contour
-            else:
-                last_grid = None
-            
-            if last_grid is not None:
-                cv2.drawContours(original_img, [last_grid], -1, (0, 255, 0), 3)
-                return last_grid
-            return None
     
+    if largest_contour is not None:
+        last_grid = largest_contour
+    else:
+        last_grid = None
+    
+    if last_grid is not None:
+        cv2.drawContours(original_img, [last_grid], -1, (0, 255, 0), 3)
+        return last_grid
+    return None
+
+def reorder_points(pts):
+    pts = pts.reshape((4,2))
+    new_pts = np.zeros((4,2), dtype = np.float32)
+
+    s = pts.sum(axis = 1)
+    new_pts[0] = pts[np.argmin(s)]
+    new_pts[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis = 1)
+    new_pts[1] = pts[np.argmin(diff)]  # Fixed from overwriting `new_pts`
+    new_pts[3] = pts[np.argmax(diff)]  # Fixed from overwriting `new_pts`
+
+    return new_pts
+
+def warped_perspective(img, corners):
+    if corners is None:
+        return None
+    ordered_corners = reorder_points(corners)
+    target_corners = np.array([[0, 0], [450, 0], [450, 450], [0, 450]], dtype=np.float32)
+
+    transform = cv2.getPerspectiveTransform(ordered_corners, target_corners)
+    warped = cv2.warpPerspective(img, transform, (450, 450))
+
+    return warped
 
 while True:
     ret, img = cap.read()
@@ -54,4 +76,17 @@ while True:
     img_copy = img.copy()
     grid_corners = getContours(imgCanny, img_copy)
 
+    warped_grid = None
+    if grid_corners is not None:
+        warped_grid = warped_perspective(img, grid_corners)
 
+    cv2.imshow('Webcam', img_copy)
+
+    if warped_grid is not None:
+        cv2.imshow('Bird Eye View', warped_grid)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
