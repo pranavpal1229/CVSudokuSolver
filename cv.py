280cb937ec0d1756cdee75fb03438376356d1819
Pranav <pranavpal@uchicago.edu>
2025-02-21 02:14:51 -0600

    working contours

--- cv.py
+++ cv.py
@@ -37,5 +37,21 @@ def getContours(img, original_img):
             return None
     
 
+while True:
+    ret, img = cap.read()
+    if not ret:
+        break
+    
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5,5), 3) #smooting out the values
+
+    imgThresh = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    kernel = np.ones((3, 3), np.uint8)
+    imgMorph = cv2.morphologyEx(imgThresh, cv2.MORPH_CLOSE, kernel, iterations=2) #not really sure what it does but GPT recommended it and it's working
+
+    imgCanny = cv2.Canny(imgMorph, 50, 150) #detects edges with lower and upper bound...might need higher upper bound
+
+    img_copy = img.copy()
+    grid_corners = getContours(imgCanny, img_copy)
 
 
